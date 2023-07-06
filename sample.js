a2bfaccda35e6a1364a03c830b25bec434453b9f
unknown <anagandula.soumya@gmail.com>
2023-07-06 22:28:37 +0530

    Add existing project files to Git

---
+++ sample.js
@@ -0,0 +1,15 @@
+function displayText() {
+    var text = document.getElementById("textField");
+    text.style.display = "block";
+  }
+
+function displayText2() {
+    var text = document.getElementById("textField2");
+    text.style.display = "block";
+  }
+
+  function displayText3() {
+    var text = document.getElementById("textField3");
+    text.style.display = "block";
+  }
+
